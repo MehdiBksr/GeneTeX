bfcd4e3885ff5878de0300a88a1cbec8ced25a7a
Mehdi Bouksara <mehdi.bouksara@gmail.com>
2013-05-29 20:43:16 +0200

    Test package

--- GeneTeX/src/GeneTeX.java
+++
@@ -1,8 +0,0 @@
-class GeneTeX {
-	public static void main(String[] args) {
-		System.out.print("GeneTeX args : ");
-		for(int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
-		}
-	}
-}
---
+++ GeneTeX/src/Main/GeneTeX.java
@@ -0,0 +1,15 @@
+package Main;
+/**
+ * This is the main class of the program.
+ * <p>
+ * 
+ * @return None.
+ */
+public class GeneTeX {
+	public static void main(String[] args) {
+		System.out.print("GeneTeX args : ");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
