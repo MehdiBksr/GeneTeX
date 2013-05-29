80f5cef773d1a984d87cd68b189eb6ebaaa6c633
Mehdi Bouksara <mehdi.bouksara@gmail.com>
2013-05-29 18:37:38 +0200

    Test code

--- GeneTeX/src/GeneTeX.java
+++ GeneTeX/src/GeneTeX.java
@@ -1,5 +1,8 @@
 class GeneTeX {
 	public static void main(String[] args) {
-		System.out.println("GeneTeX");
+		System.out.print("GeneTeX args : ");
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
 	}
 }
