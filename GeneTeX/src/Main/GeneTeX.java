f040e2d715ff7393a88c2f0bf9611743a3a5b61d
Marceau Thalgott <marceau.thalgott@gmail.com>
2013-05-31 14:02:13 +0200

    Test Marceau

--- GeneTeX/src/Main/GeneTeX.java
+++ GeneTeX/src/Main/GeneTeX.java
@@ -7,7 +7,7 @@ package Main;
  */
 public class GeneTeX {
 	public static void main(String[] args) {
-		System.out.print("GeneTeX args : ");
+		System.out.print("GeneTeX args (Marceau) : ");
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
