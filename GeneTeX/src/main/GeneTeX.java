4cb01532812e871ab939658357daaeeda5374a5a
Mehdi Bouksara <mehdi.bouksara@gmail.com>
2013-06-03 12:17:00 +0200

    Hop

---
+++ GeneTeX/src/main/GeneTeX.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * This is the main class of the program.
+ * <p>
+ * 
+ * @return None.
+ */
+public class GeneTeX {
+	public static void main(String[] args) {
+		System.out.print("GeneTeX args (Marceau) : ");
+		System.out.println("LOLOLOL");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
