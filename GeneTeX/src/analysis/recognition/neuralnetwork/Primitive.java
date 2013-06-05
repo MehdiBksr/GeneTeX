52bf30e7b0c06e143591a406c649240281f4cc9b
Théo Merle <theo.merle@ensimag.fr>
2013-06-05 15:21:38 +0200

    added GeneTeX/TemplateCodingStyle.txt for coding style examples.

--- GeneTeX/src/analysis/recognition/neuralnetwork/Primitive.java
+++ GeneTeX/src/analysis/recognition/neuralnetwork/Primitive.java
@@ -14,7 +14,36 @@ public class Primitive implements Layer {
      *                              METHODS                                   * 
      ************************************************************************ */
     
-    /* ************************************************************************
+	/**
+	 * Return the number of primitives
+	 * @return number of primitives contained in the layer
+	 */
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Return the value of the index-th primitive computed. If no primitive
+	 * corresponds to index, it returns 0.
+	 * <ul>
+	 * <li> index = 0 corresponds to standardisationCoefficientX
+	 * <li> index = 1 corresponds to standardisationCoefficientY
+	 * <li> index = 2..33 corresponds to upOutline
+	 * <li> index = 34..65 corresponds to downOutline
+	 * <li> index = 66..97 corresponds to rightOutline
+	 * <li> index = 98..129 corresponds to leftOutline
+	 * </ul>
+	 *
+	 * @param index an integer corresponding to a primitive.
+	 * @return      value of the selected primitive (0 by default).
+	 */
+	public float getValue(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* ************************************************************************
      *                          PRIVATE FUNCTIONS                             * 
      ************************************************************************ */
     
