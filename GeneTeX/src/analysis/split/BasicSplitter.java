211c58f158d08ac6cf3e598f12550fa566ad2fc2
Mehdi Bouksara <mehdi.bouksara@gmail.com>
2013-06-06 10:02:30 +0200

    Merge

--- GeneTeX/src/analysis/split/BasicSplitter.java
+++ GeneTeX/src/analysis/split/BasicSplitter.java
@@ -2,7 +2,9 @@ package analysis.split;
 
 import java.util.Vector;
 
+import data.PreprocessedImage;
 import data.imagedata.*;
+import error.split.StructuredInSplittedException;
 
 public class BasicSplitter implements Splitter {
     
@@ -10,31 +12,36 @@ public class BasicSplitter implements Splitter {
      *                              METHODS                                   * 
      ************************************************************************ */
 	
-	public SplittedPage primarySegmentation(boolean[][] preprocessedPage) {
+	public SplittedPage primarySegmentation(PreprocessedImage preprocessedPage) {
 		SplittedPage page = new SplittedPage();
 		Vector<SplittedBlock> blocks = splitInBlocks(preprocessedPage);
 		// Our image has only one block here
-		page.addBlock(blocks.get(0));
-		// We look for the lines in the page		
-		int lineIndex;
-		for (lineIndex = 0; lineIndex < preprocessedPage[0].length; lineIndex++) {
-			if (countPixels(preprocessedPage[0]) != 0) {
-				
-			}
+		try {
+			page.addBlock(blocks.get(0));
+		} catch (StructuredInSplittedException e) {
+			
+			e.printStackTrace();
 		}
+		// We look for the lines in the page		
+		
 		
 		return page;		
 	}
 	
-	public Vector<SplittedBlock> splitInBlocks(boolean[][] preprocessedPage) {
+	public Vector<SplittedBlock> splitInBlocks(PreprocessedImage preprocessedPage) {
+		Vector<SplittedBlock> blocks = new Vector<SplittedBlock>();
+		SplittedBlock onlyBlock = new SplittedBlock();
+		blocks.add(onlyBlock);
+		
+		return blocks;
+	}
+	
+	public Vector<SplittedLine> splitInLines(PreprocessedImage preprocessedBlock) {
+		
 		return null;
 	}
 	
-	public Vector<SplittedLine> splitInLines(boolean[][] preprocessedBlock) {
-		return null;
-	}
-	
-	public Vector<SplittedSymbol> splitInSymbols(boolean[][] preprocessedLine) {
+	public Vector<SplittedSymbol> splitInSymbols(PreprocessedImage preprocessedLine) {
 		return null;
 	}
     
@@ -50,7 +57,7 @@ public class BasicSplitter implements Splitter {
 		return pixelCount;
 	}
 	
-	private int lookForLine(boolean[][] preprocessedPage) {
+	private int lookForLine(PreprocessedImage image) {
 		return 0;
 		
 	}
